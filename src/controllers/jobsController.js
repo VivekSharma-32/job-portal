ec2b9975d2b0b959b24bf2f69b90d4de04fe3059
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-04-10 10:51:20 +0530

    stats and filters added

--- src/controllers/jobsController.js
+++ src/controllers/jobsController.js
@@ -1,4 +1,6 @@
 import jobsModel from "../models/jobsModel.js";
+import mongoose from "mongoose";
+import moment from "moment";
 
 // create new job
 export const createJob = async (req, res, next) => {
@@ -21,3 +23,116 @@ export const getAllJobs = async (req, res, next) => {
     jobs,
   });
 };
+
+// ======== UPDATE JOBS ====
+export const updateJobController = async (req, res, next) => {
+  const { id } = req.params;
+  const { companyName, position, createdBy } = req.body;
+
+  // validation
+  if (!companyName || !position) {
+    next("Please provide all fields");
+  }
+  // find job
+  const job = await jobsModel.findOne({ _id: id });
+
+  // validation
+  if (!job) {
+    next(`No job found with this id ${id}`);
+  }
+
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("You are not authorized to update this job");
+    return;
+  }
+
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({ updateJob });
+};
+
+//  delete a job
+
+export const deleteJobController = async (req, res, next) => {
+  const { id } = req.params;
+  // find job
+  const job = await jobsModel.findOne({ _id: id });
+
+  // validation
+  if (!job) {
+    next(`No job found by this id ${id}`);
+  }
+
+  if (!req.user.userId === job.createdBy.toString()) {
+    next(`You are not authorized to delete this job`);
+  }
+
+  await job.deleteOne();
+
+  res.status(200).json({ message: "Success, Job deleted" });
+};
+
+// ========= JOBS STATS AND FILTER ==========
+export const jobStatsController = async (req, res) => {
+  const stats = await jobsModel.aggregate([
+    // search by user doc
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    {
+      $group: {
+        _id: "$status",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+  const defaultStats = {
+    pending: stats.pending || 0,
+    reject: stats.reject || 0,
+    interview: stats.interview || 0,
+  };
+
+  // monthly yearly stats
+  let monthlyApplication = await jobsModel.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { $month: "$createdAt" },
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+
+  monthlyApplication = monthlyApplication
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+
+  res
+    .status(200)
+    .json({ totalJobs: stats.length, defaultStats, monthlyApplication });
+};
+
+// default stats
--- src/routes/jobsRoutes.js
+++ src/routes/jobsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import userAuth from "./../middlewares/authMiddleware.js";
-import { createJob, getAllJobs } from "./../controllers/jobsController.js";
+import {
+  createJob,
+  deleteJobController,
+  getAllJobs,
+  jobStatsController,
+  updateJobController,
+} from "./../controllers/jobsController.js";
 
 // router object
 const router = express.Router();
@@ -11,5 +17,14 @@ router.post("/create-job", userAuth, createJob);
 
 // GET JOBS || GET
 router.get("/get-jobs", userAuth, getAllJobs);
+
+// UPDATE JOBS || UPDATE
+router.patch("/update-job/:id", userAuth, updateJobController);
+
+// DELETE JOB || DELETE
+router.delete("/delete-job/:id", userAuth, deleteJobController);
+
+// JOBS STATS || FILTER
+router.get("/job-stats", userAuth, jobStatsController);
 
 export default router;
