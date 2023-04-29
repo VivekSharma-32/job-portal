21d7633f775d80e80308ddb8145f5cacdf6fed01
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2023-04-29 11:59:15 +0530

    changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import "../styles/HomePage.css";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-primary">Home Page</h1>
-    </div>
+    <>
+      <video autoPlay muted loop id="myVideo">
+        <source src="/assets/videos/bg.mp4" type="video/mp4" />
+      </video>
+      <div className="content">
+        <div className="card w-25">
+          <img src="/assets/images/logo/logo.png" alt="" />
+          <hr />
+          <div className="card-body" style={{ marginTop: "-60px" }}>
+            <h5>India's No #1 Career Platform</h5>
+            <p className="card-text">
+              Search and manage your jobs with ease. Free and open source job
+              portal application
+            </p>
+            <div className="d-flex justify-content-between mt-5">
+              <p>
+                Not a user. <Link to="/register">Register</Link>
+              </p>
+              <p>
+                <Link to="/login" className="myBtn">
+                  Login
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
